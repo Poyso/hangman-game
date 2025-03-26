fbe0f1b9c18eb8e389ebabfb29f15160ab5d3294
Poyso <costantini039@gmail.com>
2025-03-26 11:07:01 +0100

    Input template implemented

--- include/Buttons.h
+++ include/Buttons.h
@@ -1,6 +1,7 @@
 #ifndef Button_h
 #define Button_h
 #include <SFML/Graphics.hpp>
+#include <optional>
 
 // Centering the text
 const float text_offset_x = 25;
@@ -15,32 +16,35 @@ const float default_side = 70.f;
 class myButton {
 
 public:
-  myButton(sf::Text t, sf::RectangleShape rectangle) : _text(t) {
-    _text = t;
+  myButton() {}
+  myButton(sf::Text t, sf::RectangleShape rectangle)
+      : _text(std::make_optional(t)) {
     _rectangle = rectangle;
   }
   void SetColor(sf::Color rect_color, sf::Color text_color) {
-    _text.setFillColor(text_color);
+    _text.value().setFillColor(text_color);
     _rectangle.setFillColor(rect_color);
   }
   void SetSize() {
     _rectangle.setSize({default_side, default_side});
-    _text.setCharacterSize(default_size_char);
+    _text.value().setCharacterSize(default_size_char);
   }
   void SetPos(sf::Vector2f ShapePos, sf::Vector2f TextPos) {
     _rectangle.setPosition(ShapePos);
-    _text.setPosition(TextPos);
+    _text.value().setPosition(TextPos);
   }
-  void SetText(const sf::String string) { _text.setString(string); }
-  sf::String GetString() { return _text.getString(); }
-  sf::Text GetText() { return _text; }
-  sf::RectangleShape GetShape() { return _rectangle; }
+  void SetText(const sf::String string) { _text.value().setString(string); }
+  sf::String GetString() { return _text.value().getString(); }
+  sf::Text &GetText() { return _text.value(); }
+  sf::RectangleShape &GetShape() { return _rectangle; }
 
 private:
-  sf::Text _text;
+  std::optional<sf::Text> _text;
   sf::RectangleShape _rectangle;
 };
 
-myButton CreateButton(float x, float y, sf::String letter);
+myButton CreateButton(const float x, const float y, sf::String letter);
+
+void template_buttons();
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,11 @@
 #include "Buttons.h"
 #include <SFML/Graphics.hpp>
-#include <iostream>
+
+static sf::Font ttf(font_name);
 
 int main() {
   auto window =
       sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-  sf::Font ttf(font_name);
-  sf::RectangleShape rect;
-  sf::Text text(ttf);
-  myButton test_btn(text, rect);
-  test_btn.SetSize();
-  test_btn.SetPos({50.f, 50.f}, {50.f + text_offset_x, 50.f + text_offset_y});
-  test_btn.SetText("a");
-  test_btn.SetColor(sf::Color::White, sf::Color::Black);
-  myButton btn = CreateButton(200, 200, "b");
-
   while (window.isOpen()) {
     while (const std::optional event = window.pollEvent()) {
       if (event->is<sf::Event::Closed>()) {
@@ -22,11 +13,6 @@ int main() {
       }
     }
     window.clear();
-    // window.draw(test_btn.GetShape());
-    // window.draw(test_btn.GetText());
-    std::cout << "test" << std::endl;
-    window.draw(btn.GetShape());
-    window.draw(btn.GetText());
     window.display();
   }
 }
